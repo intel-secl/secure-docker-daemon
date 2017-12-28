6192710b1b5b98da9462e295efefc2c864b1c754
root <ganeshx.navgire@intel.com>
2017-12-28 11:42:24 +0530

    moving all files to v0 branch

--- docker-security/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#set -x
-
-echo "Building and installing docker binaries ........."
-hack/make.sh binary || exit 1
-cp bundles/latest/binary-client/docker* /usr/bin/ || exit 1 
-cp bundles/latest/binary-daemon/docker* /usr/bin/ || exit 1
-echo "************* COMPILATION IS COMPLETED **************"
