9611c1c6358f43bc838aae04736499ec562550bc
R, Praveenraj <praveenraj.r@intel.com>
2019-05-12 04:39:13 -0700

    v1.0/secureovelay-patch-docker-17.06: removed docker source files

---
+++ Makefile
@@ -0,0 +1,28 @@
+VERSION := v1.0
+GITCOMMIT := $(shell git describe --always)
+GITBRANCH := $(shell git rev-parse --abbrev-ref HEAD)
+TIMESTAMP := $(shell date --iso=seconds)
+DOCKER_CE_CLI := docker-ce/components/cli
+DOCKER_CE_ENGINE := docker-ce/components/engine
+DOCKER_BUILD := out
+
+.PHONY: all clean
+
+installer:
+	mkdir out
+	chmod +x build-secure-docker-dameon.sh
+	./build-secure-docker-dameon.sh
+	cp -f $(DOCKER_CE_CLI)/build/docker-linux-amd64 $(DOCKER_BUILD)/docker
+	cp -f $(DOCKER_CE_ENGINE)/bundles/17.06.2-ce/binary-daemon/dockerd $(DOCKER_BUILD)/
+	cp -f $(DOCKER_CE_ENGINE)/bundles/17.06.2-ce/binary-daemon/docker-init $(DOCKER_BUILD)/
+	cp -f $(DOCKER_CE_ENGINE)/bundles/17.06.2-ce/binary-daemon/docker-proxy $(DOCKER_BUILD)/
+	cp -f $(DOCKER_CE_ENGINE)/bundles/17.06.2-ce/binary-daemon/docker-runc $(DOCKER_BUILD)/
+	cp -f $(DOCKER_CE_ENGINE)/bundles/17.06.2-ce/binary-daemon/docker-containerd $(DOCKER_BUILD)/
+	cp -f $(DOCKER_CE_ENGINE)/bundles/17.06.2-ce/binary-daemon/docker-containerd-shim $(DOCKER_BUILD)/
+	cp -f $(DOCKER_CE_ENGINE)/bundles/17.06.2-ce/binary-daemon/docker-containerd-ctr $(DOCKER_BUILD)/
+
+all: installer
+
+clean:
+	rm -rf out/
+	rm -rf docker-ce/
---
+++ build-secure-docker-dameon.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DOCKER_CLI=`pwd`/docker-cli/
+GRAPHDRIVER=`pwd`/graphdriver
+SECURE_OVERLAY_DIR=`pwd`/secureoverlay2
+DOCKER_BUILD=`pwd`/out
+DEPS_DIR=`pwd`/vendor
+DOCKER_CE=`pwd`/docker-ce
+BUILD_DIR=`pwd`
+
+git clone https://github.com/docker/docker-ce.git
+cd docker-ce
+git checkout 17.06
+git pull
+cd $BUILD_DIR
+
+if [ $? -ne 0 ];
+then
+  echo "Error while pulling docker engine, exiting .."
+  exit 1
+fi
+
+echo "Building docker client"
+DOCKER_CE_CLI=docker-ce/components/cli
+DOCKER_CE_ENGINE=docker-ce/components/engine
+
+cp -f $DOCKER_CLI/build.go $DOCKER_CE_CLI/cli/command/image/ 
+cp -f $DOCKER_CLI/client.go $DOCKER_CE_CLI/vendor/github.com/docker/docker/api/types/
+cp -f $DOCKER_CLI/Dockerfile.dev $DOCKER_CE_CLI/dockerfiles/Dockerfile.dev
+
+make --directory=$DOCKER_CE_CLI -f docker.Makefile binary
+
+if [ $? -ne 0 ];
+then
+  echo "Error while building docker cli, exiting"
+  exit 1
+fi
+
+cp -f $GRAPHDRIVER/register_overlay.go $DOCKER_CE_ENGINE/daemon/graphdriver/register/register_overlay.go
+cp -f $GRAPHDRIVER/driver_linux.go $DOCKER_CE_ENGINE/daemon/graphdriver/driver_linux.go
+cp -f docker-engine/Dockerfile $DOCKER_CE_ENGINE/
+cp -rf $SECURE_OVERLAY_DIR $DOCKER_CE_ENGINE/daemon/graphdriver/
+cp -rf $DEPS_DIR/rp.intel.com $DOCKER_CE_ENGINE/vendor/
+
+echo "Building docker daemon.."
+make --directory=$DOCKER_CE_ENGINE
+
+if [ $? -ne 0 ];
+then
+  echo "Error while building docker daemon, exiting"
+  exit 1
+fi
+
+echo "Successfully built docker binaries"
